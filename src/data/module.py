61382a871ecec7c0fa135802ac09bcc62fdb4843
Bartosz Stachowiak <sbartekt@op.pl>
2024-10-30 14:34:05 +0100

    create lightning data module

---
+++ src/data/module.py
@@ -0,0 +1,53 @@
+import lightning as pl
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+from torchvision.datasets import FashionMNIST
+
+import src.consts
+import src.data.preprocess
+
+
+class MNISTDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        batch_size: int,
+        num_workers: int,
+        data_dir: str = str(src.consts.DATA_PATH),
+    ):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+        self.transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081,)),
+            ]
+        )
+
+        self.dims = (1, 28, 28)
+        self.num_classes = 10
+
+    def setup(self, stage=None):
+        # Assign train/val datasets for use in dataloaders
+        if stage == "fit" or stage is None:
+            mnist_full = FashionMNIST(
+                self.data_dir, train=True, transform=self.transform, download=True
+            )
+            self.mnist_train, self.mnist_val = random_split(mnist_full, [55000, 5000])
+
+        # Assign test dataset for use in dataloader(s)
+        if stage == "test" or stage is None:
+            self.mnist_test = FashionMNIST(
+                self.data_dir, train=False, transform=self.transform, download=True
+            )
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(self.mnist_train, batch_size=self.batch_size, shuffle=True)
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(self.mnist_val, batch_size=self.batch_size, shuffle=False)
+
+    def test_dataloader(self) -> DataLoader:
+        return DataLoader(self.mnist_test, batch_size=self.batch_size, shuffle=False)
